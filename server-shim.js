c3fffe41e4294461a33c3caeea668e5da8343996
jefBinomed <jean.francois.garreau@gmail.com>
2015-07-30 15:47:17 +0200

    reset api

--- server-shim.js
+++ server-shim.js
@@ -7,10 +7,10 @@ var app = express();
 app.all('/oauthproxy', oauthshim);
 
 var creds = {
-	'139abc4e310f8adaf6bb' : '505849b65b3d971e54730401082f437eba810037'
-}
-//creds[credentials.TWITTER_CLIENT] = credentials.TWITTER_CLIENT_SECRET;
-//creds[credentials.GITHUB_CLIENT] = credentials.GITHUB_CLIENT_SECRET;
+};
+
+creds[credentials.TWITTER_CLIENT] = credentials.TWITTER_CLIENT_SECRET;
+creds[credentials.GITHUB_CLIENT] = credentials.GITHUB_CLIENT_SECRET;
 
 
 oauthshim.init(creds);
