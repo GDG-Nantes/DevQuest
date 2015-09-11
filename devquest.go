3a740f333f58ff5a413a23bdfc240b062667fb73
jlandure <julien.landure@gmail.com>
2015-09-11 17:37:21 +0200

    feat(#29): appengine

---
+++ devquest.go
@@ -0,0 +1,17 @@
+package devquest
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/v1/questions", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    //w.Header().Set("SUPER-HACK", "@GDGNANTES")
+    //w.WriteHeader(http.StatusFound)
+    fmt.Fprint(w, "Hello, world!")
+    return
+}
