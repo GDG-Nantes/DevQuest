a3d54edb1e0c818e4105ef759fac30b8ef2486d8
jefBinomed <jean.francois.garreau@gmail.com>
2015-09-23 22:37:02 +0200

    fix(#24)

--- devquest.go
+++ devquest.go
@@ -44,11 +44,14 @@ type SpreadSheet struct {
 }
 
 type Question struct{
+    Id int `json:"id"`
     Title string `json:"title"`
     RepA string `json:"repA"`
     RepB string `json:"repB"`
     RepC string `json:"repC"`
     RepD string `json:"repD"`
+    Code string `json:"-"`
+    Resp string `json:"-"`
 }
 
 type Questions struct{
@@ -74,6 +77,9 @@ func init() {
         log.Print("Error:",e)
     }
     log.Print(jsontype)
+
+
+
 }
 
 func questions(w http.ResponseWriter, r *http.Request) {
@@ -82,7 +88,7 @@ func questions(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
     client := urlfetch.Client(c)
 
-    // Get the item from the memcache
+    // Mise en cache direct du spreadsheet
     keyMemCache := "questions"+jsontype.SPREADSHEET_KEY;
     item, err := memcache.Get(c, keyMemCache);        
     if  err == memcache.ErrCacheMiss || err != nil{
@@ -90,7 +96,7 @@ func questions(w http.ResponseWriter, r *http.Request) {
         url := "https://spreadsheets.google.com/feeds/cells/"+jsontype.SPREADSHEET_KEY+"/od6/public/basic?alt=json";
         resp, err := client.Get(url)
 
-         if err != nil {
+        if err != nil {
             fmt.Fprintf(w, "%s\n", err)
             os.Exit(1)
         } else {
@@ -107,28 +113,37 @@ func questions(w http.ResponseWriter, r *http.Request) {
             indexQuestion :=0
             indexRow := 1;
             for _,element := range data.Feed.Entry{
-                keyA := "A"+strconv.Itoa(indexRow)
-                if element.Title.T == keyA{
-                    question := Question{}
+                keyId := "A"+strconv.Itoa(indexRow)
+                if element.Title.T == keyId{
+                    idTmp, _ := strconv.Atoi(element.Content.T)
+                    question := Question{
+                        Id: idTmp,
+                    }
                     questions.Questions[indexQuestion] = question
                 }else{
                     question := &questions.Questions[indexQuestion]                 
 
-                    keyB := "B"+strconv.Itoa(indexRow)
-                    keyC := "C"+strconv.Itoa(indexRow)
-                    keyD := "D"+strconv.Itoa(indexRow)
-                    keyE := "E"+strconv.Itoa(indexRow)
-                    keyF := "F"+strconv.Itoa(indexRow)
-                    if element.Title.T == keyB{
+                    keyQuestionTitle := "B"+strconv.Itoa(indexRow)
+                    keyRepA := "C"+strconv.Itoa(indexRow)
+                    keyRepB := "D"+strconv.Itoa(indexRow)
+                    keyRepC := "E"+strconv.Itoa(indexRow)
+                    keyRepD := "F"+strconv.Itoa(indexRow)
+                    keyCode := "G"+strconv.Itoa(indexRow)
+                    keyResp := "H"+strconv.Itoa(indexRow)
+                    if element.Title.T == keyQuestionTitle{
                         question.Title = element.Content.T;
-                    }else if element.Title.T == keyC{
+                    }else if element.Title.T == keyRepA{
                         question.RepA = element.Content.T;
-                    }else if element.Title.T == keyD{
+                    }else if element.Title.T == keyRepB{
                         question.RepB = element.Content.T;
-                    }else if element.Title.T == keyE{
+                    }else if element.Title.T == keyRepC{
                         question.RepC = element.Content.T;
-                    }else if element.Title.T == keyF{
+                    }else if element.Title.T == keyRepD{
                         question.RepD = element.Content.T;
+                    }else if element.Title.T == keyCode{
+                        question.Code = element.Content.T;
+                    }else if element.Title.T == keyResp{
+                        question.Resp = element.Content.T;
                         indexRow++
                         indexQuestion++
                     }
@@ -145,52 +160,87 @@ func questions(w http.ResponseWriter, r *http.Request) {
             } else if err != nil {
                 c.Errorf("error adding itemValue: %v", err)
             }
+
+            // On ajout au datastore les questions pour plus de facilité d'intérogation
+            for _,question := range questions.Questions{
+                keyQuestion := datastore.NewKey(c,"Question", strconv.Itoa(question.Id), 0, nil)
+                _, err := datastore.Put(c, keyQuestion, &question)
+                if  err != nil{
+                    // Print error
+                    fmt.Fprintf(w, "%s\n", err)
+                    // TODO jeter une erreur
+                    return
+                }
+            }
             fmt.Fprintf(w, "%s\n", strJson)
         }
-        //c.Infof("item not in the cache")
-        //c.Errorf("error getting item: %v", err)
-    } else {
+    } else{
+        
         fmt.Fprintf(w, "%s\n", item.Value)
-        // Data in Cache
-        //c.Infof("the lyric is %q", item.Value)
-    }
+    }   
     
 }
+
 
 func anwser(w http.ResponseWriter, r *http.Request) {
     //w.Header().Set("SUPER-HACK", "@GDGNANTES")
     //w.WriteHeader(http.StatusFound)
-    fmt.Fprint(w, "Hello World\n")
-    fmt.Fprint(w, jsontype.SPREADSHEET_KEY);
-    fmt.Fprint(w, "\n")
+    //fmt.Fprint(w, "Hello World\n")
+    //fmt.Fprint(w, jsontype.SPREADSHEET_KEY);
+    //fmt.Fprint(w, "\n")
     email := r.FormValue("email")
     indexQuestion := r.FormValue("indexQuestion")
     resp := r.FormValue("resp")
     code := r.FormValue("code")
     time := r.FormValue("time")
-    if len(email) != 0{
-        fmt.Fprint(w, email+"\n")
+    if len(email) == 0 || len(indexQuestion) == 0 || len(resp) == 0 || len(code) == 0 || len(time) == 0{
+        fmt.Fprint(w, "Parametre manquant\n")    
+        // TODO jetter une erreur
+        return
     }
-    if len(indexQuestion) != 0{
-        fmt.Fprint(w, indexQuestion+"\n")
+    fmt.Fprint(w, email+"\n")
+    fmt.Fprint(w, time+"\n")
+    fmt.Fprint(w, code+"\n")
+    fmt.Fprint(w, resp+"\n")
+    fmt.Fprint(w, indexQuestion+"\n")
+
+
+    c := appengine.NewContext(r)    
+    var scoreQuestion int8
+    scoreQuestion = 0
+    // Vérification du code de réponse
+    // Get the item from the memcache
+    var question Question
+    keyQuestion := datastore.NewKey(c, "Question", indexQuestion, 0, nil)
+    if err := datastore.Get(c, keyQuestion, &question); err != nil{
+        fmt.Fprintf(w, "%s\n", err)
+        // TODO jeter une erreur
+        return
     }
-    if len(resp) != 0{
-        fmt.Fprint(w, resp+"\n")
+    if resp == question.Resp{
+        scoreQuestion += 10
     }
-    if len(code) != 0{
-        fmt.Fprint(w, code+"\n")
-    }
-    if len(time) != 0{
-        fmt.Fprint(w, time+"\n")
+    if code == question.Code{
+        scoreQuestion += 5
     }
 
-    c := appengine.NewContext(r)
-    respDataStore := Resp{
-        Email: email,
-        Score: 1,
-        Time: 1,
+    // Préparation du Data Store
+    key := datastore.NewKey(c,"Score", email, 0, nil)
+
+
+
+    var respDataStore Resp
+    if err := datastore.Get(c, key, &respDataStore); err != nil{
+        // Data pas déjà présente => on la créé
+        respDataStore = Resp{
+            Email: email,
+            Time: 0,
+            Score: 0,
+        }
     }
-    key := datastore.NewIncompleteKey(c,"Score", nil)
+    timeTmp, _ :=strconv.ParseInt(time, 10, 64)
+    respDataStore.Time += timeTmp
+    respDataStore.Score +=scoreQuestion
     _, err := datastore.Put(c, key, &respDataStore)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
