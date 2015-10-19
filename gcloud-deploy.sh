e41832407039aabdf47723852709ce578ed7d2e0
Julien Landuré <julien.landure@gmail.com>
2015-10-19 15:24:14 +0200

    Update gcloud-deploy.sh

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,5 +1,6 @@
+google-cloud-sdk/bin/gcloud config set app/use_appengine_api false
 echo $GAE_KEY_FILE_CONTENT > file.key
 echo "Authenticate with $GAE_SERVICE_ACCOUNT"
 google-cloud-sdk/bin/gcloud auth activate-service-account $GAE_SERVICE_ACCOUNT --key-file file.key
-echo "Deploying to devfest"
+echo "Deploying..."
 google-cloud-sdk/bin/gcloud --project devquest2015 preview app deploy --version v2 --quiet dist/app.yaml
